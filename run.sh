5ee708ffe8f72acc74099ee892bc972b7ed35410
minouei <mohammad.minooee@gmail.com>
2021-03-12 11:22:42 +0100

    fix

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-git install git+file:///netscratch/minouei/report/tf/TensorFlow-Advanced-Segmentation-Models
+pip install git+file:///netscratch/minouei/report/tf/TensorFlow-Advanced-Segmentation-Models
 python deeplabv3.py
