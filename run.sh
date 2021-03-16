0568ace731e23d80daf23189403ef7a27022c90d
minouei <mohammad.minooee@gmail.com>
2021-03-16 01:20:25 +0100

    dl3

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-python -m pip cache remove opencv-python
-python -m pip cache purge
 pip install git+file:///netscratch/minouei/report/tf/TensorFlow-Advanced-Segmentation-Models
+mv /usr/local/lib/python3.8/dist-packages/cv2/cv2.cpython-38-x86_64-linux-gnu.so cv2.so
 python slurm_deeplabv3.py
