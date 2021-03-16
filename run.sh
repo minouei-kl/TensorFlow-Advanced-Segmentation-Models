f65d0ea0f7f825f7da92fcd68a45cfb38bd00102
minouei <mohammad.minooee@gmail.com>
2021-03-16 10:55:05 +0100

    dl3

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 pip install git+file:///netscratch/minouei/report/tf/TensorFlow-Advanced-Segmentation-Models
 export LD_LIBRARY_PATH=/usr/local/lib/python3.8/dist-packages/opencv_python_headless.libs:$LD_LIBRARY_PATH
+pip install opencv-python-headless==3.4.*
+ldconfig
+export LD_LIBRARY_PATH=/usr/local/lib/python3.8/dist-packages/opencv_python_headless.libs:$LD_LIBRARY_PATH
 python slurm_deeplabv3.py
